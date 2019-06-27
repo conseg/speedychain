8b760b1234d9706f14c3e638c4e2330758a8daa5
Eduardo Arruda <eduardo.arruda@doc.space>
2019-06-27 09:06:41 -0300

    Cria script de teste

---
+++ src/run-devices.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+python2.7 Device.py 50.16.193.245 9090 gateway-us-east-1 100 50 &
+python2.7 Device.py 50.16.193.245 9090 gateway-us-west-2 100 50 &
+python2.7 Device.py 50.16.193.245 9090 gateway-ap-northeast-1 100 50 &
+python2.7 Device.py 50.16.193.245 9090 gateway-us-east-1 100 50 &
+python2.7 Device.py 50.16.193.245 9090 gateway-us-west-2 100 50 &
+python2.7 Device.py 50.16.193.245 9090 gateway-ap-northeast-1 100 50 &
