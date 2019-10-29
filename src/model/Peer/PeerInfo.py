04e61ee4d8255c5916783ed9fafa5a6120e4034a
LucasGCardoso <lucasgavirachicc@gmail.com>
2019-10-29 15:24:25 -0300

    Deletando pastas e arquivos duplicados

--- src/model/Peer/PeerInfo.py
+++
@@ -1,4 +0,0 @@
-class PeerInfo:
-    def __init__(self, peerURI,  object):
-        self.peerURI = peerURI
-        self.object = object
--- src/tools/DeviceKeyMapping.py
+++
@@ -1,4 +0,0 @@
-class DeviceKeyMapping:
-    def __init__(self, publicKey, AESKey):
-        self.publicKey = publicKey
-        self.AESKey = AESKey
