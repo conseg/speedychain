599a16ab802db39a7aa78e12cf3643833876b1f1
regio <regiom@gmail.com>
2019-11-13 10:35:47 +1100

    changing labels

--- src/tools/launcher.sh
+++ src/tools/launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R1 dev-a true; exec bash\""
 sleep 1
-gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R1 dev-a true; exec bash\""
+gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R2 dev-b true; exec bash\""
 sleep 1
-gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R1 dev-a true; exec bash\""
+gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R3 dev-c true; exec bash\""
