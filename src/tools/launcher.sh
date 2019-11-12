b791b68da9027184cbc5e251bfb80d937ad9e1ae
regio <regiom@gmail.com>
2019-11-13 10:04:30 +1100

    script to launch multiple stream handlers

---
+++ src/tools/launcher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R1 dev-a true; exec bash\""
+sleep 1
+gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R1 dev-a true; exec bash\""
+sleep 1
+gnome-terminal -e "bash -c \"python StreamHandler.py 129.94.175.201 12666 R1 dev-a true; exec bash\""
