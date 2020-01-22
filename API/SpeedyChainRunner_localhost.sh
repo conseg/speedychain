e408b24b34711d16f54fcf1a47dff5f59fd682c5
LucasGCardoso <lucasgavirachicc@gmail.com>
2020-01-22 16:43:57 -0300

    Adding commands on API runner script

--- API/SpeedyChainRunner_localhost.sh
+++ API/SpeedyChainRunner_localhost.sh
@@ -3,13 +3,13 @@ export PATH=$PATH:/usr/local/go/bin
 echo $GOPATH /home/roben/go
 gnome-terminal -e "bash -c \"python -m Pyro4.naming -n 127.0.0.1 -p 9090; exec bash\""
 sleep 1
-gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py 127.0.0.1 9090 gwa 0001; exec bash\""
+gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py -n 127.0.0.1 -p 9090 -G gwa -C 0001; exec bash\""
 sleep 1
-gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py 127.0.0.1 9090 gwb 0001; exec bash\""
+gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py -n 127.0.0.1 -p 9090 -G gwb -C 0001; exec bash\""
 sleep 1
-gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py 127.0.0.1 9090 gwc 0001; exec bash\""
+gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py -n 127.0.0.1 -p 9090 -G gwc -C 0001; exec bash\""
 sleep 1
-gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py 127.0.0.1 9090 gwd 0001; exec bash\""
+gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/runner.py -n 127.0.0.1 -p 9090 -G gwd -C 0001; exec bash\""
 sleep 1
 #gnome-terminal -e "bash -c \"python ~/speedychain_varruda/speedychain-master/src/tools/DeviceSimulator.py 127.0.0.1 9090 gwa dev-a     50   10 PoW; exec bash\""
 gnome-terminal -e "bash -c \"python ~/PycharmProjects/speedychain/API/src/tools/DeviceSimulator.py 127.0.0.1 9090 gwa dev-a; exec bash\""
